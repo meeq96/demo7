08b901fab7f8977157fb120ad90560c9468998eb
meeq96 <quachhongthu131@.gmail.com>
2019-07-06 21:15:58 +0700

    add file

---
+++ demo.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $('.click1').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:$('#services').offset().top
+        },1000)
+    });
+    $('.click2').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:$('#statistics').offset().top
+        },1000)
+    });
+    $('.click3').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:$('#partners').offset().top
+        },1000)
+    });
+    $('.click4').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:$('#why').offset().top
+        },1000)
+    });
+    $('.click5').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:$('#team').offset().top
+        },1000)
+    });
+    var check=true
+    if(check===true){
+        $('.drop').click(function (e) { 
+            e.preventDefault();
+            $('.txt').css('display','block')
+        });
+        check=false
+    }
+    else{
+        $('.drop').click(function (e) { 
+            e.preventDefault();
+            $('.txt').css('display','none')
+        });
+        check=true
+    }
+});
